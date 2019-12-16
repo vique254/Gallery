a6f7f8514d436d94ae0a4054469bee2518cfd313
vique254 <victoronyango051@gmail.com>
2019-12-16 14:39:25 +0300

    create form

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.main,name = 'welcome'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import location,category,Image
 from django.http  import HttpResponse
 # Create your views her
-def welcome(request):
+def main(request):
     return render(request,'index.html')
     
