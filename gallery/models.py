d83055bc3a2e5e329f4df9cfb71011f7bfdf1977
vique254 <victoronyango051@gmail.com>
2019-12-16 10:38:18 +0300

    create second and third class models

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,17 @@ from django.db import models
 class Image(models.Model):
    image_name = models.CharField(max_length=30)
    image_description = models.CharField(max_length=100)
+   image_location = models.ForeignKey(location)
+   image_category = models.ForeignKey(category)
    
+   
+class  location(models.Model):
+    place = models.CharField(max_length=35)
+    
+    def __str__(self):
+        return self.place
+    
+class   category(models.Model):
+    name = models.CharField(max_length=30) 
+    image = models.ForeignKey(Image)
+    location = models.ForeignKey(location)
