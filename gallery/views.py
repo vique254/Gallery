1508e6768e0a56af9a47215fa770319f60ee82b3
vique254 <victoronyango051@gmail.com>
2019-12-16 08:06:26 +0300

    rename welcome template

--- gallery/views.py
+++ gallery/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 # Create your views her
 def welcome(request):
-    return HttpResponse('Welcome to Gall')
+    return render(request,'welcome.html')
     
