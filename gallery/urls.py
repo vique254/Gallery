4155e416a966d8f14ae77eaf44af4deb1efb9f28
vique254 <victoronyango051@gmail.com>
2019-12-16 16:23:58 +0300

    change route paths

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,5 +7,5 @@ urlpatterns=[
     url('^$',views.main,name = 'welcome'),
     url(r'^search/',views.search_results,name='search_results')
 ]
-if  settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
