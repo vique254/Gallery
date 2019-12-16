6dae4245dd69d179a5cce436677914cd408a6794
vique254 <victoronyango051@gmail.com>
2019-12-16 09:03:04 +0300

    change project database settings

--- galler/settings.py
+++ galler/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'galler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'galler',
+        'USER':'moringa',
+        'PASSWORD':'vique',
     }
 }
 
