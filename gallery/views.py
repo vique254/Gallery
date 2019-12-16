da6bab6fc663b1ada44afff3bd7e31e689222162
vique254 <victoronyango051@gmail.com>
2019-12-16 15:05:55 +0300

    create search_results view function

--- gallery/views.py
+++ gallery/views.py
@@ -4,4 +4,15 @@ from django.http  import HttpResponse
 # Create your views her
 def main(request):
     return render(request,'index.html')
-    
+
+def search_results(request):
+    if  'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categories =category.search_by_name(search_term)
+        message = f"{search_term}"
+        
+        return render (request,'search.html',{"message":message,"category":searched_categories})
+
+    else:
+        message = "You haven't searched for any term" 
+        return render(request,'search.html',{"message":message})   
